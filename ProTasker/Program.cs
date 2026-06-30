2e2478466f41af840d337697d3c96e76cb42bfd4
Roman Buchynskyi <168230669+dramonRog@users.noreply.github.com>
2026-06-30 09:13:47 +0200

    feat: modern scalar tool for testing end-points

--- ProTasker/Program.cs
+++ ProTasker/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using ProTasker.Data;
 using ProTasker.Services;
 using ProTasker.Validators.Project;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +27,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
