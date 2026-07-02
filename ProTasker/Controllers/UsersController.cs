8ecb262c6651cf24c6db0bc5144794d3b43572ca
Roman Buchynskyi <168230669+dramonRog@users.noreply.github.com>
2026-07-02 09:41:01 +0200

    feat: UsersController

---
+++ ProTasker/Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProTasker.DTOs.Requests.User;
+using ProTasker.DTOs.Responses.User;
+using ProTasker.Services;
+
+namespace ProTasker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<List<UserResponse>> GetAllUsers(CancellationToken cancellationToken)
+        {
+            return await _userService.GetAllAsync(cancellationToken);
+        }
+
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
+        public async Task<ActionResult<UserResponse>> GetUserById(Guid id, CancellationToken cancellationToken)
+        {
+            UserResponse? response = await _userService.GetByIdAsync(id, cancellationToken);
+
+            if (response == null)
+                return NotFound();
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
+        public async Task<ActionResult<UserResponse>> UpdateUser(Guid id, UpdateUserRequest request, CancellationToken cancellationToken)
+        {
+            UserResponse? response = await _userService.UpdateUserAsync(id, request, cancellationToken);
+
+            if (response == null)
+                return NotFound();
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteUserById(Guid id, CancellationToken cancellationToken)
+        {
+            bool result = await _userService.DeleteByIdAsync(id, cancellationToken);
+
+            return result ? NoContent() : NotFound();
+        }
+    }
+}
