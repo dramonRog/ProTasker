40c8cad936454defcdd85a7d8157fdd528d44a31
Roman Buchynskyi <168230669+dramonRog@users.noreply.github.com>
2026-07-14 20:34:04 +0200

    test: add unit tests for TaskComment validators

---
+++ ProTasker.UnitTests/TaskComment/CreateTaskCommentRequestValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentValidation.TestHelper;
+using ProTasker.DTOs.Requests.TaskComment;
+using ProTasker.Validators.TaskComment;
+using Xunit;
+
+namespace ProTasker.UnitTests.Validators.TaskComment
+{
+    public class CreateTaskCommentRequestValidatorTests
+    {
+        private readonly CreateTaskCommentRequestValidator _validator;
+
+        public CreateTaskCommentRequestValidatorTests()
+        {
+            _validator = new CreateTaskCommentRequestValidator();
+        }
+
+        [Fact]
+        public void Should_NotHaveError_When_ModelIsValid()
+        {
+            var request = new CreateTaskCommentRequest("Valid Title", "Valid Description");
+            var result = _validator.TestValidate(request);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void Should_HaveError_When_TitleIsEmpty(string? invalidTitle)
+        {
+            var request = new CreateTaskCommentRequest(invalidTitle!, "Valid Description");
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Title)
+                  .WithErrorMessage("Title can't be empty.");
+        }
+
+        [Fact]
+        public void Should_HaveError_When_TitleIsTooLong()
+        {
+            var request = new CreateTaskCommentRequest(new string('a', 101), "Valid Description");
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Title)
+                  .WithErrorMessage("Title can't contain more than 100 characters.");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void Should_HaveError_When_DescriptionIsEmpty(string? invalidDescription)
+        {
+            var request = new CreateTaskCommentRequest("Valid Title", invalidDescription!);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Description)
+                  .WithErrorMessage("Description can't be empty.");
+        }
+
+        [Fact]
+        public void Should_HaveError_When_DescriptionIsTooLong()
+        {
+            var request = new CreateTaskCommentRequest("Valid Title", new string('a', 2001));
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Description)
+                  .WithErrorMessage("Comment is too long (max 2000 characters).");
+        }
+    }
+}
---
+++ ProTasker.UnitTests/TaskComment/UpdateTaskCommentRequestValidatorTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentValidation.TestHelper;
+using ProTasker.DTOs.Requests.TaskComment;
+using ProTasker.Validators.TaskComment;
+using Xunit;
+
+namespace ProTasker.UnitTests.Validators.TaskComment
+{
+    public class UpdateTaskCommentRequestValidatorTests
+    {
+        private readonly UpdateTaskCommentRequestValidator _validator;
+
+        public UpdateTaskCommentRequestValidatorTests()
+        {
+            _validator = new UpdateTaskCommentRequestValidator();
+        }
+
+        [Fact]
+        public void Should_HaveErrorForObject_When_AllFieldsAreNull()
+        {
+            var request = new UpdateTaskCommentRequest(null, null);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x)
+                  .WithErrorMessage("Submit at least one field to update.");
+        }
+
+        [Fact]
+        public void Should_NotHaveError_When_OnlyTitleIsProvided()
+        {
+            var request = new UpdateTaskCommentRequest("Updated Title", null);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldNotHaveValidationErrorFor(x => x);
+            result.ShouldNotHaveValidationErrorFor(x => x.Title);
+        }
+
+        [Fact]
+        public void Should_NotHaveError_When_OnlyDescriptionIsProvided()
+        {
+            var request = new UpdateTaskCommentRequest(null, "Updated Description");
+            var result = _validator.TestValidate(request);
+
+            result.ShouldNotHaveValidationErrorFor(x => x);
+            result.ShouldNotHaveValidationErrorFor(x => x.Description);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void Should_HaveError_When_TitleIsProvidedButEmpty(string invalidTitle)
+        {
+            var request = new UpdateTaskCommentRequest(invalidTitle, null);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Title)
+                  .WithErrorMessage("Title can't be empty.");
+        }
+
+        [Fact]
+        public void Should_HaveError_When_TitleIsTooLong()
+        {
+            var request = new UpdateTaskCommentRequest(new string('a', 101), null);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Title)
+                  .WithErrorMessage("Title can't contain more than 100 characters.");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void Should_HaveError_When_DescriptionIsProvidedButEmpty(string invalidDescription)
+        {
+            var request = new UpdateTaskCommentRequest(null, invalidDescription);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Description)
+                  .WithErrorMessage("Comment can't be empty.");
+        }
+
+        [Fact]
+        public void Should_HaveError_When_DescriptionIsTooLong()
+        {
+            var request = new UpdateTaskCommentRequest(null, new string('a', 2001));
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Description)
+                  .WithErrorMessage("Comment is too long (max 2000 characters).");
+        }
+    }
+}
