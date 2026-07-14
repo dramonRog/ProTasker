6e44338f80390d4d617f77f9c3a84ffb480cb67a
Roman Buchynskyi <168230669+dramonRog@users.noreply.github.com>
2026-07-14 20:38:43 +0200

    test: add unit tests for ProjectMember validators

---
+++ ProTasker.UnitTests/Validators/ProjectMember/AddProjectMemberRequestValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.TestHelper;
+using ProTasker.DTOs.Requests.ProjectMember;
+using ProTasker.Validators.ProjectMember;
+using Xunit;
+using System;
+
+namespace ProTasker.UnitTests.Validators.ProjectMember
+{
+    public class AddProjectMemberRequestValidatorTests
+    {
+        private readonly AddProjectMemberRequestValidator _validator;
+
+        public AddProjectMemberRequestValidatorTests()
+        {
+            _validator = new AddProjectMemberRequestValidator();
+        }
+
+        [Fact]
+        public void Should_NotHaveError_When_UserIdIsValid()
+        {
+            var request = new AddProjectMemberRequest(Guid.NewGuid());
+            var result = _validator.TestValidate(request);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.UserId);
+        }
+
+        [Fact]
+        public void Should_HaveError_When_UserIdIsEmpty()
+        {
+            var request = new AddProjectMemberRequest(Guid.Empty);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId)
+                  .WithErrorMessage("Select a user to add to this project.");
+        }
+    }
+}
---
+++ ProTasker.UnitTests/Validators/ProjectMember/ChangeProjectMemberRoleRequestValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.TestHelper;
+using ProTasker.DTOs.Requests.ProjectMember;
+using ProTasker.Models.Enums;
+using ProTasker.Validators.ProjectMember;
+using Xunit;
+
+namespace ProTasker.UnitTests.Validators.ProjectMember
+{
+    public class ChangeProjectMemberRoleRequestValidatorTests
+    {
+        private readonly ChangeProjectMemberRoleRequestValidator _validator;
+
+        public ChangeProjectMemberRoleRequestValidatorTests()
+        {
+            _validator = new ChangeProjectMemberRoleRequestValidator();
+        }
+
+        [Fact]
+        public void Should_NotHaveError_When_RoleIsValid()
+        {
+            var request = new ChangeProjectMemberRole((ProjectRole)1);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Role);
+        }
+
+        [Fact]
+        public void Should_HaveError_When_RoleIsInvalid()
+        {
+            var request = new ChangeProjectMemberRole((ProjectRole)999);
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.Role)
+                  .WithErrorMessage("Invalid role specified.");
+        }
+    }
+}
